b392564bcfd2a7031b79fe256ef20adc06d755c2
Adolfo Correia <adolfogsc@gmail.com>
2021-02-09 15:21:08 -0300

    Initial commit

---
+++ .config/fish/config.fish
@@ -0,0 +1,11 @@
+set fish_greeting
+
+#fish_vi_key_bindings
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /opt/miniconda/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
+
+starship init fish | source
+
