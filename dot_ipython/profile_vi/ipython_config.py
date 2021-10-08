c231ba4559430e489df768ffb3277a9ec8bbf9cb
Adolfo Correia <adolfo.correia@bndes.gov.br>
2021-10-07 21:39:15 -0300

    Add ipython vi profile

---
+++ dot_ipython/profile_vi/ipython_config.py
@@ -0,0 +1,8 @@
+## Autoindent IPython code entered interactively.
+#  Default: True
+c.InteractiveShell.autoindent = False
+
+## Shortcut style to use at the prompt. 'vi' or 'emacs'.
+#  Default: 'emacs'
+c.TerminalInteractiveShell.editing_mode = 'vi'
+
