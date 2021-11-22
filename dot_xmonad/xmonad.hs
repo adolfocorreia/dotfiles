25c70fbce04433c591fdcd03092d72de0eb882fc
Adolfo Correia <adolfogsc@gmail.com>
2021-11-21 21:22:20 -0300

    XMonad/xmobar improvements

--- dot_xmonad/xmonad.hs
+++ dot_xmonad/xmonad.hs
@@ -15,12 +15,12 @@ Partial type signature reference:
 
 -- Imports
 
-import Control.Lens (indices)
 import qualified Data.Map as M
 import Data.Maybe
-import System.IO (Handle, hPutStrLn)
+import System.IO
 import XMonad
 import XMonad.Actions.CycleWS
+import XMonad.Actions.DwmPromote
 import XMonad.Actions.GroupNavigation
 import XMonad.Config.Desktop
 import XMonad.Hooks.DynamicLog
@@ -30,6 +30,7 @@ import XMonad.Layout.LayoutModifier
 import XMonad.Layout.Renamed
 import XMonad.Layout.ResizableTile
 import XMonad.Layout.Spacing
+import qualified XMonad.StackSet as W
 import XMonad.Util.EZConfig
 import XMonad.Util.Run
 import XMonad.Util.SpawnOnce
@@ -45,9 +46,11 @@ myRed = "#bf616a"
 myWhite = "#d8dee9"
 myYellow = "#ebcb8b"
 
-myCurrentColor, myVisibleColor :: String
-myCurrentColor = "#81a1c1"
+myCurrentColor, myVisibleColor, myHiddenColor, myHiddenNoWindowsColor :: String
+myCurrentColor = myPink
 myVisibleColor = "#8fbcbb"
+myHiddenColor = myWhite
+myHiddenNoWindowsColor = "#4c566a"
 
 -- Base settings
 myModMask :: KeyMask
@@ -57,13 +60,27 @@ myTerminal :: String
 myTerminal = "alacritty"
 
 -- Custom key bindings
--- Reference: https://github.com/xmonad/xmonad/blob/master/src/XMonad/Config.hs
-myKeys :: [(String, X ())]
-myKeys =
+-- References:
+-- - https://github.com/xmonad/xmonad/blob/master/src/XMonad/Config.hs
+-- - https://hackage.haskell.org/package/xmonad-contrib/docs/XMonad-Util-EZConfig.html
+myAdditionalKeys :: [(String, X ())]
+myAdditionalKeys =
   [ ("M-p", spawn "rofi -show"),
     ("M-\\", spawn "rofi -show"),
     -- Close the focused window
     ("M-S-x", kill),
+    -- Toggle workspace layout
+    ("M-S-<Space>", sendMessage NextLayout),
+    -- Workspace 10/0 bindings
+    ("M-0", windows $ W.greedyView $ myWorkspaces !! (10 - 1)),
+    ("M-S-0", windows $ W.shift $ myWorkspaces !! (10 - 1)),
+    -- TODO: add F1-12 workspaces
+    -- Volume controls
+    ("<XF86AudioLowerVolume>", spawn "amixer -q sset Master 5%- unmute"),
+    ("<XF86AudioRaiseVolume>", spawn "amixer -q sset Master 5%+ unmute"),
+    ("<XF86AudioMute>", spawn "amixer set Master toggle"),
+    -- Swap master windows
+    ("M-<Return>", dwmpromote),
     -- Switch focus to next monitor
     ("M-C-h", prevScreen),
     ("M-C-l", nextScreen),
@@ -78,15 +95,15 @@ myKeys =
     ("M-S-<Tab>", moveTo Prev NonEmptyWS)
   ]
 
+myRemoveKeys :: [String]
+myRemoveKeys =
+  [ -- Win+Space is used to toggle keyboard layouts
+    "M-<Space>"
+  ]
+
 -- Startup processes
 myStartupHook :: X ()
 myStartupHook = do
-  -- TODO: move X commands to X configuration files
-  -- Monitor configuration (check monitors with 'xrandr --listmonitors')
-  spawnOnce
-    "xrandr --output DP-0 --primary --left-of DP-3 --output DP-3 --auto &"
-  -- Keyboard configuration: CapsLock as Ctrl, both Shifts to toggle CapsLock
-  spawnOnce "setxkbmap -option ctrl:nocaps,shift:both_capslock &"
   -- Session manager / polkit authentication agent
   spawnOnce "lxsession --session XMonad &"
   -- Desktop background
@@ -100,26 +117,26 @@ myLayoutHook = avoidStruts (tall ||| full)
   where
     tall =
       renamed [Replace "tall"] $ spc $ ResizableTall 1 (5 / 100) (60 / 100) []
-    full = renamed [Replace "full"] $ Full
+    full = renamed [Replace "full"] Full
     spc = spacingRaw False border True border True
     border = Border gap gap gap gap
-    gap = 4
+    gap = 8
 
 -- Workspaces configuration
 myWorkspaces :: [String]
 myWorkspaces = ["term", "www", "dev", "doc", "file", "mail", "chat", "mus", "vid", "conf"]
 
 workspaceIcon :: String -> String
-workspaceIcon "term" = "<fn=2>\xf07b7</fn>"
-workspaceIcon "www" = "<fn=2>\xf059f</fn>"
 workspaceIcon "dev" = "<fn=2>\xf05c0</fn>"
 workspaceIcon "doc" = "<fn=2>\xf06d3</fn>"
-workspaceIcon "file" = "<fn=2>\xf0770</fn>"
-workspaceIcon "mail" = "<fn=2>\xf02ab</fn>"
-workspaceIcon "chat" = "<fn=2>\xf0b79</fn>"
 workspaceIcon "mus" = "<fn=2>\xf075a</fn>"
 workspaceIcon "vid" = "<fn=2>\xf05a0</fn>"
+workspaceIcon "www" = "<fn=2>\xf059f</fn>"
+workspaceIcon "chat" = "<fn=2>\xf0b79</fn>"
 workspaceIcon "conf" = "<fn=2>\xf0493</fn>"
+workspaceIcon "file" = "<fn=2>\xf0770</fn>"
+workspaceIcon "mail" = "<fn=2>\xf02ab</fn>"
+workspaceIcon "term" = "<fn=2>\xf07b7</fn>"
 workspaceIcon str = ""
 
 -- Windows management
@@ -134,10 +151,14 @@ myManageHook =
     myClassFloats = ["mpv", "SpeedCrunch", "vlc"]
     myTitleFloats = []
 
+-- TODO: add window/workspaces routing rules (firefox, brave, emacs, vscode, lyx, whatsapp, libreoffice)
+
 -- Xmobar configuration
-clickable :: (WorkspaceId, String) -> String
-clickable (ws, icon) = "<action=xdotool key super+" ++ show index ++ ">" ++ icon ++ "</action>"
+-- Reference:  https://hackage.haskell.org/package/xmonad-contrib/docs/XMonad-Hooks-DynamicLog.html
+clickableWS :: WorkspaceId -> String
+clickableWS ws = "<action=`xdotool key super+" ++ show index ++ "`>" ++ icon ++ "</action>"
   where
+    icon = workspaceIcon ws
     index = fromJust $ M.lookup ws indices
     indices = M.fromList $ zip myWorkspaces $ [1 .. 9] ++ [0]
 
@@ -146,29 +167,32 @@ myXmobarConfig xm0 xm1 =
   xmobarPP
     { ppOutput = \x -> hPutStrLn xm0 x >> hPutStrLn xm1 x,
       ppSep = " | ",
-      ppCurrent = xmobarColor myCurrentColor "" . prepareWS,
-      ppVisible = xmobarColor myVisibleColor "" . prepareWS,
-      ppHidden = prepareWS,
+      ppCurrent = xmobarColor myCurrentColor "" . boxed myCurrentColor . clickableWS,
+      ppVisible = xmobarColor myVisibleColor "" . boxed myVisibleColor . clickableWS,
+      ppHidden = xmobarColor myHiddenColor "" . clickableWS,
+      ppHiddenNoWindows = xmobarColor myHiddenNoWindowsColor "" . clickableWS,
       ppUrgent = xmobarColor myRed myYellow . wrap "!" "!",
-      ppTitle = xmobarColor myCurrentColor ""
+      ppLayout = clickableLayout,
+      ppTitle = xmobarColor myCurrentColor "" . boxed myCurrentColor
     }
   where
-    prepareWS ws = clickable (ws, workspaceIcon ws)
+    clickableLayout = wrap "<action=`xdotool key super+shift+space`>" "</action>"
+    boxed color = wrap ("<box type=Bottom width=3 color=" ++ color ++ ">") "</box>"
 
 -- Main
 main :: IO ()
 main = do
-  xm0 <- spawnPipe "xmobar -x 0"
-  xm1 <- spawnPipe "xmobar -x 1"
+  xm0 <- spawnPipe "xmobar -x 0 ~/.config/xmobar/xmobarrc_primary"
+  xm1 <- spawnPipe "xmobar -x 1 ~/.config/xmobar/xmobarrc_secondary"
   xmonad $
     docks $
       ewmh
         desktopConfig
           { modMask = myModMask,
             terminal = myTerminal,
-            focusedBorderColor = myWhite,
+            focusedBorderColor = myCurrentColor,
             normalBorderColor = myBlack,
-            borderWidth = 2,
+            borderWidth = 3,
             workspaces = myWorkspaces,
             startupHook = myStartupHook,
             layoutHook = myLayoutHook,
@@ -176,4 +200,5 @@ main = do
             handleEventHook = handleEventHook def <+> docksEventHook,
             logHook = dynamicLogWithPP (myXmobarConfig xm0 xm1)
           }
-        `additionalKeysP` myKeys
+        `removeKeysP` myRemoveKeys
+        `additionalKeysP` myAdditionalKeys
