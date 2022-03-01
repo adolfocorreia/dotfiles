19888fe53e6d07a549d3c5fb2e227f31f9c77498
Adolfo Correia <adolfogsc@gmail.com>
2022-03-01 13:42:35 -0300

    lf: initial commit (linux)

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -28,13 +28,9 @@ if status is-interactive
         set fish_vi_force_cursor
     end
 
-    # Set environment variables
-    set EDITOR nvim
-
     # Set aliases
     alias ls=exa
     alias vi=nvim
-    alias open=handlr open
 
     # Load conda
     # Reference: https://stackoverflow.com/questions/34280113/add-conda-to-path-in-fish
