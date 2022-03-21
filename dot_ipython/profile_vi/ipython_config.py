2be00dac810d9418da716f7dbc19ee519d10080e
Adolfo Correia <adolfogsc@gmail.com>
2022-03-21 03:28:51 -0300

    ipython: improve colors

--- dot_ipython/profile_vi/ipython_config.py
+++ dot_ipython/profile_vi/ipython_config.py
@@ -1,3 +1,9 @@
+# pylint: disable=C0111
+c = c  # noqa: F821 pylint: disable=E0602,C0103,W0127
+
+## Configuration reference:
+## - https://ipython.readthedocs.io/en/stable/config/options/terminal.html
+
 ## Autoindent IPython code entered interactively.
 #  Default: True
 c.InteractiveShell.autoindent = False
@@ -6,3 +12,6 @@ c.InteractiveShell.autoindent = False
 #  Default: 'emacs'
 c.TerminalInteractiveShell.editing_mode = 'vi'
 
+## Use 24bit colors instead of 256 colors in prompt highlighting.
+# Default: False
+c.TerminalInteractiveShell.true_color = True
