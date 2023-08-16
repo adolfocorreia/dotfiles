72accf71f7004e62507706fcdc47efef83bd8cdd
Adolfo Correia <adolfogsc@gmail.com>
2023-08-16 11:53:11 -0300

    kanata: adjust executable PATH

--- dot_config/kanata/kanata.bat
+++ dot_config/kanata/kanata.bat
@@ -1,2 +1,2 @@
 :: Run with 'conhost.exe --headless' to inhibit the console window
-start /MIN /HIGH /B %APPS%\kanata\kanata.exe --cfg %USERPROFILE%\.config\kanata\kanata.doublewide.kbd
+start /MIN /HIGH /B kanata.exe --cfg %USERPROFILE%\.config\kanata\kanata.doublewide.kbd
