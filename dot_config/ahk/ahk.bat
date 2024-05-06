77b03fbd753d5478385d23f87aae4c184b24a460
Adolfo Correia <adolfogsc@gmail.com>
2024-05-06 11:51:19 -0300

    ahk: adjust command path

--- dot_config/ahk/ahk.bat
+++ dot_config/ahk/ahk.bat
@@ -1,1 +1,1 @@
-start /MIN /ABOVENORMAL /B autohotkey32.exe %USERPROFILE%\.config\ahk\script.ahk
+start /MIN /ABOVENORMAL /B %USERPROFILE%\AppData\Local\Programs\AutoHotkey\v2\autohotkey32.exe %USERPROFILE%\.config\ahk\script.ahk
