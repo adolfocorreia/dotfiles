1933450e4807fafc03e986086b71c55ce7c142a0
Adolfo Correia <adolfogsc@gmail.com>
2025-01-06 10:29:51 -0300

    emacs: fix git install root path

--- dot_emacs.d/early-init.el
+++ dot_emacs.d/early-init.el
@@ -51,4 +51,4 @@
 
 (let
    ((git-home (getenv "GIT_INSTALL_ROOT")))
-   (if git-home (add-to-list 'exec-path git-home)))
+   (if git-home (add-to-list 'exec-path (expand-file-name "cmd" git-home))))
