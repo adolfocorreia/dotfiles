39d408b5ec64c26951c1f10c5294ec8ce1f60034
Adolfo Correia <adolfogsc@gmail.com>
2025-02-02 21:46:49 -0300

    fish: add options flag to wttr function

--- dot_config/fish/functions/wttr.fish
+++ dot_config/fish/functions/wttr.fish
@@ -1,20 +1,27 @@
 # Reference: https://github.com/chubin/wttr.in
 function wttr --description "Prints weather information from wttr.in"
-    set --local options 'h/help' 'v/version=!_validate_int --min 1 --max 3'
+    set spec 'h/help' 'v/version=!_validate_int --min 1 --max 3' 'o/options=?'
 
-    argparse $options -- $argv
-    or return
+    argparse $spec -- $argv
+    or return 1
 
     if set --query _flag_help
         curl "wttr.in/:help"
         return 0
     end
 
-    set --query _flag_version; or set --local _flag_version 1
+    set --query _flag_version; or set _flag_version 1
 
-    set --local location (string replace ' ' '+' (string join '+' $argv))
+    set --query _flag_options; or set _flag_options 'F'
 
-    set --local url (string join '' v $_flag_version .wttr.in/ $location '?F')
+    if test (count $argv) -gt 0
+        set location $argv
+    else
+        read location < "$XDG_CONFIG_HOME/wttr/default-city.txt"
+    end
+    set location (string replace --all ' ' '+' (string join '+' $location))
+
+    set url (string join '' 'v' $_flag_version '.wttr.in/' $location '?' $_flag_options)
 
     curl $url
 end
