ec17edb5bd422ba438fa4336b9d2a0404b9fd45f
Adolfo Correia <adolfogsc@gmail.com>
2025-03-02 09:33:59 -0300

    Use Intel for Xorg and NVIDIA for cuda

--- dot_config/aconfmgr/05-kernel.sh
+++ dot_config/aconfmgr/05-kernel.sh
@@ -3,9 +3,16 @@ AddPackage linux # The Linux kernel and modules
 AddPackage linux-lts # The LTS Linux kernel and modules
 AddPackage linux-firmware # Firmware files for Linux
 AddPackage intel-ucode # Microcode update files for Intel CPUs
-AddPackage nvidia # NVIDIA kernel modules
+
+# Video drivers and tools
+AddPackage intel-gpu-tools # Tools for development and testing of the Intel DRM driver
+AddPackage intel-media-driver # Intel Media Driver for VAAPI — Broadwell+ iGPUs
+AddPackage intel-media-sdk # Legacy API for hardware video acceleration on Intel GPUs (Broadwell to Rocket Lake)
 AddPackage nvidia-lts # NVIDIA drivers for linux-lts
+AddPackage nvidia # NVIDIA kernel modules
 AddPackage nvidia-settings # Tool for configuring the NVIDIA graphics driver
+
+CopyFile /etc/X11/xorg.conf.d/10-intel-xorg-nvidia-cuda.conf
 
 # Boot
 AddPackage efibootmgr # Linux user-space application to modify the EFI Boot Manager
