adc9af1c70946bc492102b3795c61248c8fade66
Adolfo Correia <adolfogsc@gmail.com>
2025-10-27 18:59:57 -0300

    qutebrowser: close config file

--- AppData/Roaming/qutebrowser/config/config.py
+++ AppData/Roaming/qutebrowser/config/config.py
@@ -1,4 +1,5 @@
 import os
 config_home = os.getenv("XDG_CONFIG_HOME")
 assert config_home is not None
-exec(open(os.path.join(config_home, "qutebrowser/config.py")).read())
+with open(os.path.join(config_home, "qutebrowser/config.py")) as file:
+    exec(file.read())
